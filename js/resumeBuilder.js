5002da6a1f8cbf6525945656b47edde229025cf9
jnthnrzr <jonathanrozario@gmail.com>
2016-08-08 23:26:07 -0400

    Worked on resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+//  */
+// $("#main").append(["Jonathan Rozario"])
+
+var name = "Jonathan Rozario";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
